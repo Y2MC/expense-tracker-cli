878e90948dc40e5378648999a21cfe0f0f413cb5
Y2MC <chuang887739@gmail.com>
2026-09-17 14:30:16 +1200

    Initial menu and loop structure

---
+++ tracker.py
@@ -0,0 +1,31 @@
+expenses = []
+
+while True:
+
+  print("\nExpense Tracker")
+  print("1. Add Expense")
+  print("2. View Expenses")
+  print("3. Show Total")
+  print("4. Delete Expense")
+  print("5. Quit")
+
+  option = input("Select an option from 1-5:")
+
+  if option == "1":
+    print("Add Expense Selected.")
+
+  elif option =="2":
+    print("View Expenses Selected.")
+
+  elif option == "3":
+    print("Show Total Selected.")
+
+  elif option == "4":
+    print("Delete Expense Selected.")
+
+  elif option == "5":
+    print("Quit selected.")
+    break
+
+  else:
+    print("Invalid number. Please enter a number from 1-5.")
